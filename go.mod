9a58084cc5750fe955a54bfc8b3bc4db23968927
Leonardo Lima <leonardoaugusto287@gmail.com>
2021-04-18 03:24:11 -0300

    feat: .env file support

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/leozz37/gin-serverless-container-template
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.1
+require (
+	github.com/gin-gonic/gin v1.7.1
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load(".env.example")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Gin setup
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
